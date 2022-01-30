1f7f8ae5e57a6117af028a2081fdd9955a5c0390
kinleywood <kinleywood@gmail.com>
2022-01-29 17:45:44 -0700

    home page

---
+++ chamber/js/dates.js
@@ -0,0 +1,17 @@
+// Declare datefeild = element with id of date
+const datefield = document.querySelector("#date");
+
+// Get the current date using a date object
+const now = new Date ();
+const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+
+datefield.innerHTML = fulldate;
+
+// Toggle hamburger menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
