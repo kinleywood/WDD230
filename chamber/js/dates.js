41175c2bbf3c084f5594e6cd71936a7b12fd7c99
kinleywood <kinleywood@gmail.com>
2022-02-06 20:44:27 -0700

    favicon

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -22,7 +22,3 @@ lastmod.textContent = `Last Modification: ${document.lastModified}`;
 document.querySelector("#currentYear");
 currentYear.innerHTML = `&copy ${new Date().getFullYear()} Aberdeen Chamber`;
 
-let join = document.createAttribute("p");
-join.innerHTML = "Join";
-join.setAttribute("class", "placeholder join");
-document.domain.appendChild(join);
