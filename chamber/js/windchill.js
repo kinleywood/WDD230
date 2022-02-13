85b61119257a9739965bf1dbdf07fe477af9cc9c
kinleywood <kinleywood@gmail.com>
2022-02-13 14:39:12 -0700

    windchill

---
+++ chamber/js/windchill.js
@@ -0,0 +1,14 @@
+// temperature
+let t = parseInt(document.querySelector("#temperature").innerText);
+// wind speed
+let s = parseInt(document.querySelector("#windSpeed").innerText);
+
+if (t <= 50 && s > 3) {
+    // calculate the windchill
+    let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    document.querySelector("#windChill").innerHTML = `${Math.round(f)}&degF`;
+}
+else {
+    document.querySelector("#windChill").innerText = "N/A";
+}
+
