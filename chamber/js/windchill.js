876dd78bd48b1d2b4452e5e0c0080c72ac1bb8d8
kinleywood <kinleywood@gmail.com>
2022-02-13 14:46:30 -0700

    html clean up

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -6,7 +6,7 @@ let s = parseInt(document.querySelector("#windSpeed").innerText);
 if (t <= 50 && s > 3) {
     // calculate the windchill
     let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    document.querySelector("#windChill").innerHTML = `${Math.round(f)}&degF`;
+    document.querySelector("#windChill").innerHTML = `${Math.round(f)}&deg;F`;
 }
 else {
     document.querySelector("#windChill").innerText = "N/A";
