7c63c70526a371399155460d12d6c1efeb44b078
kinleywood <kinleywood@gmail.com>
2022-02-19 22:05:16 -0700

    commit

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -34,6 +34,24 @@ switch (day) {
     case 3:
         document.querySelector("#meetAndGreet").style.display = "block";
     break;
-    default:
-        document.querySelector("#meetAndGreet").style.display = "none";
+//     default:
+//         document.querySelector("#meetAndGreet").style.display = "none";
 }
+
+// link the the element with and id of timeBetweenVisits
+const timeBetweenVisits = document.querySelector("#timeBetweenVisits");
+
+// get the value stored in local storage.
+let lastVisit = Date.parse(window.localStorage.getItem("visits-ls"));
+
+
+if (lastVisit !== null) { () => {
+	let dateDiff = (Date.parse(now) - lastVisit) / (1000*60*60*24);
+    
+    timeBetweenVisits.textContent = dateDiff;
+}} else {
+	timeBetweenVisits.textContent = `This is your first visit!`;
+}
+
+
+localStorage.setItem("visits-ls", now);
