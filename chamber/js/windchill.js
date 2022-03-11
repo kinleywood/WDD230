4fb8279671ce7e974e24ac043b4d07ab04a2d368
kinleywood <kinleywood@gmail.com>
2022-03-11 11:14:14 -0700

    weather api

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,14 +1,35 @@
-// temperature
-let t = parseInt(document.querySelector("#temperature").innerText);
-// wind speed
-let s = parseInt(document.querySelector("#windSpeed").innerText);
+function windChill(t, s){
+    // temperature
+    // let t = parseInt(document.querySelector("#temperature").innerText);
+    // wind speed
+    // let s = parseInt(document.querySelector("#windSpeed").innerText);
 
-if (t <= 50 && s > 3) {
-    // calculate the windchill
-    let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    document.querySelector("#windChill").innerHTML = `${Math.round(f)}&deg;F`;
+    if (t <= 50 && s > 3) {
+        // calculate the windchill
+        let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+        document.querySelector("#windChill").innerHTML = `${Math.round(f)}&deg;F`;
+    }
+    else {
+        document.querySelector("#windChill").innerText = "N/A";
+    }
 }
-else {
-    document.querySelector("#windChill").innerText = "N/A";
-}
+// --------------Weather API-------------------
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5583714&units=imperial&appid=a06ca24ed79ff6ee8e5088116862388d";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const iconSrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    const temp = jsObject.main.temp;
+    const windSpeed = jsObject.wind.speed;
+    document.querySelector(".weatherIcon").setAttribute("src", iconSrc);
+    document.querySelector(".weatherIcon").setAttribute("alt", desc);
+    document.querySelector("#temperature").textContent = Math.round(temp);
+    document.querySelector(".sky").textContent = desc;
+    document.querySelector("#windSpeed").textContent = Math.round(windSpeed);
+    windChill(temp, windSpeed);
+  });
+
 
---
+++ lesson10/js/weather.js
@@ -0,0 +1,15 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5861897&units=imperial&appid=a06ca24ed79ff6ee8e5088116862388d";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
+
